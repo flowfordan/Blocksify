c507562f59d36ae66b965d6fea4863e50d8a11fd
flowfordan <flowfordan@gmail.com>
2022-05-15 19:29:42 +0300

    coords display fix

--- src/components/menus/CreateMenu/CreateMenu.tsx
+++ src/components/menus/CreateMenu/CreateMenu.tsx
@@ -1,28 +1,50 @@
 import React from 'react';
 import styles from './CreateMenu.module.css';
 
+
+interface Coords {
+    x: number;
+    y: number;
+    z: number;
+};
+
+
 export const CreateMenu = (props:any): JSX.Element => {
 
-  const {worldCoords} = props
+  const {worldCoords} = props;
+  
+  //initial values
+  let coords: Coords = {
+    x: 0,
+    y: 0,
+    z: 0
+  };
+
 
   const onCreateGeom = () => {
     props.onCrtBtn()  
   };
 
+  if(worldCoords){
+    coords.x = worldCoords.x.toFixed(2);
+    coords.y = worldCoords.z.toFixed(2);
+    coords.z = worldCoords.y.toFixed(2);
+  }
+  
   
 
   return (
 
       <div className={styles.header}>
 
-        Creation Menu
+        <h4>Creation Menu</h4>
         <div>
           <button onClick={onCreateGeom}>Create Point</button>
           <div>
             Coordinates
-            <div>{`X: ${worldCoords.x.toFixed(2)}`}</div>
-            <div>{`Y: ${worldCoords.z.toFixed(2)}`}</div>
-            <div>{`Z: ${worldCoords.y.toFixed(2)}`}</div>
+            <div>{`X: ${coords.x}`}</div>
+            <div>{`Y: ${coords.y}`}</div>
+            <div>{`Z: ${coords.z}`}</div>
           </div>
         </div>
         
