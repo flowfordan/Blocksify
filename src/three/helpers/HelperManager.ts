1af907c6504e5baf52a773d691b27ed5aa5d4b7f
flowfordan <flowfordan@gmail.com>
2022-09-07 22:57:09 +0300

    fix: separate Helper class

---
+++ src/three/helpers/HelperManager.ts
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import { sceneState, HelperOption } from '../../state';
+
+//new instance is created when Tool's startDrawing() called
+class HelperManager {
+	options: Array<HelperOption>;
+	scene: THREE.Scene;
+	grid: THREE.GridHelper | null;
+
+	constructor(scene: THREE.Scene){
+		this.options = sceneState.helpersOptions;
+		this.grid = null;
+		this.scene = scene;
+	}
+
+	renderGrid = () => {
+
+		//clean up
+		this.scene.remove(this.grid as THREE.Object3D)
+
+		const gridOptions = this.options.find(i => i.type === 'grid');
+
+		const totalSize = 5000;
+		let size = gridOptions? gridOptions.value : 10;
+		const division = totalSize/size;
+
+		this.grid = new THREE.GridHelper( 5000, division, 0x4AA8FF, 0xC3C3C3 );
+		this.grid.name = 'grid';
+
+		if(!gridOptions?.isActive){
+			this.grid.visible = false;
+		}
+
+		this.scene.add(this.grid);
+	}
+	
+}
+
+export{HelperManager}
