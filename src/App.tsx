b11d0857e54fecb645d0a5b51991a602c279a3ac
flowfordan <flowfordan@gmail.com>
2022-10-15 13:50:04 +0300

    fix: files locations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './App.module.css';
 import {BrowserRouter, Routes, Route,} from "react-router-dom";
-import {AppPage} from '../../pages/AppPage/AppPage';
-import {StartPage} from '../../pages/StartPage/StartPage';
-import {Header} from '../Header/Header';
+import {AppPage} from './pages/AppPage/AppPage';
+import {StartPage} from './pages/StartPage/StartPage';
+import {Header} from './components/Header/Header';
 
 
 const App= (): JSX.Element => {
+  console.log('ENV', process.env.NODE_ENV );
   return (
     <BrowserRouter>
       <div className={styles.wrapper}>
