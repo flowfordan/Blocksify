5970f6a92a8d8b03124a8ce98c352e8ddc74be12
flowfordan <flowfordan@gmail.com>
2022-11-04 12:09:41 +0300

    feature: assets added

--- src/components/_data/assetsData.ts
+++ src/components/_data/assetsData.ts
@@ -1,6 +1,12 @@
 import PolygonIcon from '../../assets/icons/polygon.svg';
 import PolylineIcon from '../../assets/icons/polyline.svg';
 import LineIcon from '../../assets/icons/line.svg';
+import SelectorIcon from '../../assets/icons/selector.svg';
+import HelperIcon from '../../assets/icons/helper.svg';
+
+import CameraTopIcon from '../../assets/icons/camera_top.svg';
+import CameraPerspectiveIcon from '../../assets/icons/camera_perspective.svg';
+
 import ArrowHeadIcon from '../../assets/icons/arrowHead.svg';
 import EyeIcon from '../../assets/icons/eye.svg';
 import EyeClosedIcon from '../../assets/icons/eye_closed.svg';
@@ -11,11 +17,14 @@ enum Assets {
   polygon = 'polygon',
   line = 'line',
   pLine = 'pLine',
-  arrowHead = 'arrowHead',
   selector = 'selector',
+  helper = 'helper',
+  arrowHead = 'arrowHead',
   eye = 'eye',
   eye_closed = 'eyeClosed',
   lock = 'lock',
+  cameraTop = 'cameraTop',
+  cameraPerspective = 'cameraPerspective'
 }
 
 export type AssetKey = `${Assets}`;
@@ -28,11 +37,15 @@ const assetsData: AssetsData = {
   polygon: PolygonIcon,
   pLine: PolylineIcon,
   line: LineIcon,
+  selector: SelectorIcon,
+  //
+  helper: HelperIcon,
   arrowHead: ArrowHeadIcon,
-  selector: ArrowHeadIcon,
   eye: EyeIcon,
   eyeClosed: EyeClosedIcon,
-  lock: LockIcon
+  lock: LockIcon,
+  cameraTop: CameraTopIcon,
+  cameraPerspective: CameraPerspectiveIcon
 };
 
 export { assetsData };
