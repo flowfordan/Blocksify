bb8e379b1034dcfd21fabba00ec336993a939643
Daniel Rychkov <flowfordan@gmail.com>
2023-01-18 21:08:29 +0700

    feature: cleaner class

--- src/three/tools/Cleaner.ts
+++ src/three/tools/Cleaner.ts
@@ -1,3 +1,26 @@
-export class Cleaner {}
+/* 
+Cleaner
+Instrument for scene cleaner - all scene obj or by layer
+*/
+export class Cleaner {
+  isAvialable: boolean;
+  isActive: boolean;
+  constructor() {
+    //avialable
+    //active
+    this.isAvialable = true;
+    this.isActive = false;
+  }
 
-export {};
+  //clean scene
+  cleanUp = (type: 'scene' | 'layer', layerId?: number) => {
+    if (type === 'scene') {
+      //clean scene
+      //set stage 1
+    } else if (type === 'layer' && layerId !== undefined) {
+      //clean layer
+    } else {
+      throw new Error('Cleaner. Layer ID was not defined');
+    }
+  };
+}
