df741f8a37d41ae70389d0c511f00659be9b04df
flowfordan <flowfordan@gmail.com>
2023-07-01 15:41:47 +0700

    fix: desk resize

--- src/widgets/Desk/ui/Desk.tsx
+++ src/widgets/Desk/ui/Desk.tsx
@@ -14,31 +14,18 @@ export const Desk = () => {
   const [isMounted, setIsMounted] = useState(false);
   // const [canvasSize, setCanvasSize] = useState({ width: 0, height: 0 });
   const [sceneView, setSceneView] = useState<SceneView | null>(null);
-
-  // const canvasScene = useRef<null | HTMLCanvasElement>(null);
-  const canvasUI = useRef<null | HTMLCanvasElement>(null);
   const canvasContainer = useRef<null | HTMLDivElement>(null);
 
-  const onResize = useCallback((target: HTMLCanvasElement, entry: ResizeObserverEntry) => {
-    // Handle the resize event
-    if (sceneView) {
-      console.log('fire resize', target.offsetWidth, entry.contentRect.width, entry.contentRect.height);
-      sceneView.onWindowResize(entry.contentRect.width, entry.contentRect.height);
-    }
-  }, []);
-  const canvasScene = useResizeObserver(onResize);
-  console.log(canvasScene.current);
-
-  console.log('Desk');
-
-  const resizeObserver = new ResizeObserver((entries) => {
-    entries.forEach((entry) => {
-      //on view params change
+  const onResize = useCallback(
+    (target: HTMLCanvasElement, entry: ResizeObserverEntry) => {
+      // Handle the resize event
       if (sceneView) {
         sceneView.onWindowResize(entry.contentRect.width, entry.contentRect.height);
       }
-    });
-  });
+    },
+    [sceneView]
+  );
+  const canvasScene = useResizeObserver(onResize);
 
   useEffect(() => {
     const innerTreeRef = canvasScene.current;
@@ -55,33 +42,12 @@ export const Desk = () => {
           cameraModel
         )
     );
-    // resizeObserver.observe(innerTreeRef, { box: 'content-box' });
-
-    // if (canvasContainer.current && sceneView) {
-    //   const width = canvasContainer.current.offsetWidth;
-    //   const height = canvasContainer.current.offsetHeight;
-    //   sceneView.onWindowResize(width, height);
-    //   // setCanvasSize((prevSizes) => {
-    //   //   return {
-    //   //     ...prevSizes,
-    //   //     width: width,
-    //   //     height: height,
-    //   //   };
-    //   // });
-    // }
-    console.log('MOUNTED');
   }, [isMounted]);
 
   //on Mount
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  // useEffect(() => {
-  //   if (sceneView && canvasScene.current) {
-  //     resizeObserver.observe(canvasScene.current, { box: 'content-box' });
-  //   }
-  // }, [sceneView]);
 
   return (
     <div ref={canvasContainer} className={'desk'}>
