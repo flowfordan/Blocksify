81fb2189842905c05e9f2cac17e7b16827f12a8f
flowfordan <flowfordan@gmail.com>
2023-07-01 12:37:07 +0700

    init setup

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+// import Layout from '../components/Layout';
+// import PageHome from '../components/PageHome';
+
+{
+  /* <Layout noAuth>
+<PageHome />
+</Layout> */
+}
+
+const Home: NextPage = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
---
+++ pages/workspace.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+// import Layout from '../components/Layout';
+// import PageHome from '../components/PageHome';
+
+{
+  /* <Layout noAuth>
+<PageHome />
+</Layout> */
+}
+
+const Workspace: NextPage = () => {
+  return <div>Workspace</div>;
+};
+
+export default Workspace;
