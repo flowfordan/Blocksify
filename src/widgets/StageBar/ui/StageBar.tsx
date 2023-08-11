df1cb1418162ac02fee934e91571b96162695264
flowfordan <flowfordan@gmail.com>
2023-08-11 21:48:16 +0700

    fix: app stage ui

--- src/widgets/StageBar/ui/StageBar.tsx
+++ src/widgets/StageBar/ui/StageBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StageBarProps } from './StageBar.props';
 import { DrawInstrItem, HelperInstrItem, instrumentsModel, instrumentsHelpersModel } from 'entities/sceneInstrument';
 import { observer } from 'mobx-react-lite';
@@ -8,10 +8,22 @@ import './stageBar.scss';
 import cn from 'classnames';
 
 export const StageBar = observer(({ className }: StageBarProps) => {
+  const [isBarVisible, setIsBarVisible] = useState(true);
+  const handleShowBar = () => {
+    setIsBarVisible(!isBarVisible);
+  };
   return (
     <div className={cn(className, 'stageBar')}>
-      <span className={'stageBar__btn'}>Btn</span>
-      <span className={'stageBar__bar'}>Bar</span>
+      <button className={'stageBar__btn'} onClick={() => handleShowBar()}>
+        B
+      </button>
+      <span
+        className={cn('stageBar__bar', {
+          ['stageBar__bar--active']: isBarVisible,
+        })}
+      >
+        Bar
+      </span>
     </div>
   );
 });
